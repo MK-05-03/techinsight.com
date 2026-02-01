4fcf4479c65769b01da408f0c3f29a669fcac2a6
MK-05-03 <minahilkhalid085@gmail.com>
2026-02-01 23:57:30 +0500

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,8 +1,3 @@
-
 function toggleMenu() {
     document.getElementById("navMenu").classList.toggle("show");
 }
-
-<button class="back-home" onclick="goHome()" aria-label="Back to Home">
-    ⌂
-</button>
